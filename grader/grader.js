a029e8332c073cac7627d7bba2d7d33912d700ac
Ivan Zelentsov <izelentsov@gmail.com>
2013-07-09 11:13:00 +0000

    Fixed bugs in inital version of grader.js

--- grader/grader.js
+++ grader/grader.js
@@ -28,7 +28,16 @@ var cheerio = require ('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
-var assertFileExists = function (htmlfile) {
+var assertFileExists = function (infile) {
+    var instr = infile.toString ();
+    if (!fs.existsSync (instr)) {
+	console.log ("%s does not exist. Exiting.", instr);
+	process.exit (1);  // http://nodejs.org/api/process.html#process_process_exit_code
+    }
+    return instr;
+}
+
+var cheerioHtmlFile = function (htmlfile) {
     return cheerio.load (fs.readFileSync (htmlfile));
 }
 
@@ -36,7 +45,7 @@ var loadChecks = function (checksfile) {
     return JSON.parse (fs.readFileSync (checksfile));
 }
 
-var checkHTMLFile = function (htmlfile, checksfile) {
+var checkHtmlFile = function (htmlfile, checksfile) {
     $ = cheerioHtmlFile (htmlfile);
     var checks = loadChecks (checksfile);
     var out = {};
